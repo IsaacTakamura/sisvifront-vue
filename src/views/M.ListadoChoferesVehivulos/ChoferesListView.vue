34facaedb4cf0a451269c71272fa502170db8ad0
Isaac <isaactakamura107@gmail.com>
2024-07-16 03:20:55 -0500

    Ahora ya esta terminado conductores (Listado de Choferes con sus vehiculos)

--- src/views/M.ListadoChoferesVehivulos/ChoferesListView.vue
+++ src/views/M.ListadoChoferesVehivulos/ChoferesListView.vue
@@ -1,35 +1,39 @@
+<!-- Pag.Name= ChoferesListView.vue -->
 <template>
-  <div class="col-md-3">
-    <h1 class="title">Choferes</h1>
-    <p class="subtitle">Seleccione un conductor</p>
-    <input type="text" :value="localSearch" @input="handleInput" placeholder="Nombre del Chofer" class="form-control">
+  <div>
+    <input
+      type="text"
+      :value="localSearch"
+      @input="handleInput"
+      placeholder="Nombre del Chofer"
+      class="form-control"
+    />
     <ul class="list-group">
-      <li class="list-group-item" v-for="chofer in filteredChoferes" :key="chofer.id" @click="seleccionarChofer(chofer)">
+      <li
+        class="list-group-item"
+        v-for="chofer in filteredChoferes"
+        :key="chofer.id"
+        @click="seleccionarChofer(chofer)"
+      >
         {{ chofer.primerNombre }} {{ chofer.apellidoPaterno }}
       </li>
     </ul>
-    <table class="table">
-      <thead class="table-light">
-        <tr>
-          <th>#</th>
-          <th>Nombre</th>
-          <th>Apellido</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="chofer in filteredChoferes" :key="chofer.id">
-          <td>{{ chofer.id }}</td>
-          <td>{{ chofer.primerNombre }}</td>
-          <td>{{ chofer.apellidoPaterno }}</td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['search', 'choferes'],
+  props: {
+    search: {
+      type: String,
+      required: true,
+    },
+    choferes: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
   data() {
     return {
       localSearch: this.search,
@@ -37,34 +41,26 @@ export default {
   },
   computed: {
     filteredChoferes() {
-      return this.choferes.filter(chofer => {
-        return chofer.primerNombre.toLowerCase().includes(this.localSearch.toLowerCase());
+      return this.choferes.filter((chofer) => {
+        return chofer.primerNombre
+          .toLowerCase()
+          .includes(this.localSearch.toLowerCase());
       });
-    }
+    },
   },
   methods: {
     seleccionarChofer(chofer) {
-      this.$emit('mostrar-datos', chofer);
+      this.$emit("mostrar-datos", chofer);
     },
     handleInput(event) {
       this.localSearch = event.target.value;
-      this.$emit('update:search', this.localSearch);
+      this.$emit("update:search", this.localSearch);
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
-.title {
-  font-size: 24px;
-  font-weight: bold;
-}
-
-.subtitle {
-  font-size: 16px;
-  color: #888;
-}
-
 .form-control {
   width: 100%;
   padding: 8px;
--- src/views/M.ListadoChoferesVehivulos/DatosConductorView.vue
+++ src/views/M.ListadoChoferesVehivulos/DatosConductorView.vue
@@ -1,170 +1,98 @@
+<!-- Pag.Name=DatosConductorView.vue -->
 <template>
-    
-    <div class="driver-license">
-      <div class="header">
-        <h1>Datos del Conductor</h1>
-      </div>
-
-      <div class="Datos_Conductor">
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                  <div class="center-container">
-                    <div class="portrait"></div>
-                    <!--combobox para seleccionar un numero de id para cargar los datos-->
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Seleccionar ID</option>
-                        <!--Aqui se cargara las opciones dinamicamentes-->
-                    </select>
-                  </div> 
-                </div>
-    <div class="col-md-4 container text-left">
-      <div class="conductor-identificate">
-        <span>DNI:</span>
-        <span>{{ dni }}</span>
-      </div>
-
-      <div class="container text-left driver-first-name">
-        <span>Nombre:</span>
-        <span>{{ nombre }}</span>
-      </div>
-
-      <div class="driver-last-name">
-        <span>Apellido:</span>
-        <span>{{ apellido }}</span>
-      </div>
-      
-      <div class="driver-licencia">
-        <span>Licencia:</span>
-        <span>{{ Licencia }}</span>
-      </div>
-
-      <div class="driver-category">
-        <span>Categoria:</span>
-        <span>{{ Categoria }}</span>
-      </div>
-      
+  <div class="driver-license">
+    <div class="header">
+      <h1>Datos del Conductor</h1>
     </div>
-    <div class="col-md-4 ">
-      <div class="driver-telefono">
-        <span>Telefono:</span>
-        <span>{{ Telefono }}</span>
-      </div>
-
-      <div class="second-name">
-        <span>Segundo Nombre:</span>
-        <span>{{ SegundoNombre }}</span>
-      </div>
-
-      <div class="second-last-name">
-        <span>Segundo Apellido:</span>
-        <span>{{ SegundoApellido }}</span>
-      </div>
-
-      <div class="driver-fecha-vencimiento">
-        <span>Fecha de Vencimiento:</span>
-        <span>{{ FechaVencimiento }}</span>
+    <div class="Datos_Conductor">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="center-container">
+              <div class="portrait" :style="{ backgroundImage: `url(${perfilImg})` }"></div>
+            </div>
+          </div>
+          <div class="col-md-4 container text-left">
+            <div class="conductor-identificate">
+              <span>DNI:</span>
+              <span>{{ chofer.dni }}</span>
+            </div>
+            <div class="container text-left driver-first-name">
+              <span>Nombre:</span>
+              <span>{{ chofer.primerNombre }}</span>
+            </div>
+            <div class="driver-last-name">
+              <span>Apellido:</span>
+              <span>{{ chofer.apellidoPaterno }}</span>
+            </div>
+            <div class="driver-licencia">
+              <span>Licencia:</span>
+              <span>{{ chofer.licenciaConducir }}</span>
+            </div>
+            <div class="driver-category">
+              <span>Categoria:</span>
+              <span>{{ chofer.categoriaLicencia }}</span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="driver-telefono">
+              <span>Telefono:</span>
+              <span>{{ chofer.telefono }}</span>
+            </div>
+            <div class="second-name">
+              <span>Segundo Nombre:</span>
+              <span>{{ chofer.segundoNombre }}</span>
+            </div>
+            <div class="second-last-name">
+              <span>Segundo Apellido:</span>
+              <span>{{ chofer.apellidoMaterno }}</span>
+            </div>
+            <div class="driver-fecha-vencimiento">
+              <span>Fecha de Vencimiento:</span>
+              <span>{{ new Date(chofer.fechaVencimientoLicencia).toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: 'numeric' }) }}</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
-  
-      </div>
-  
-      
-    </div>
-  </template>
-  
-  <script>
-  import perfilImg from '@/assets/styles/St_ChoferesVehiculosView/Perfil.jpg';
-  import axios from 'axios';
+</template>
 
-  export default {
-    data() {
-      return {
+<script>
+export default {
+  props: {
+    chofer: {
+      type: Object,
+      required: true,
+      default: () => ({
         dni: '',
-        nombre: '',
-        apellido: '',
-        Licencia: '',
-        Categoria: '',
-        Telefono: '',
-        SegundoNombre: '',
-        SegundoApellido: '',
-        FechaVencimiento: '',
-        perfilImg: perfilImg,
-      };
-    },
-    mounted() {
+        primerNombre: '',
+        apellidoPaterno: '',
+        licenciaConducir: '',
+        categoriaLicencia: '',
+        telefono: '',
+        segundoNombre: '',
+        apellidoMaterno: '',
+        fechaVencimientoLicencia: '',
+      })
+    }
+  },
+  data() {
+    return {
+      perfilImg: require('@/assets/styles/St_ChoferesVehiculosView/Perfil.jpg'),
+    };
+  }
+};
+</script>
 
-      //cargar los datos del chofer seleccionado
-      //con el id del combobox seleccionado
-      document.querySelector('select').addEventListener('change', (event) => {
-        const id = event.target.value;
-        axios.get(`http://localhost:8069/api/choferes/listar/${id}`)
-          .then((response) => {
-            const chofer = response.data;
-            this.dni = chofer.dni;
-            this.nombre = chofer.primerNombre;
-            this.primerNombre = chofer.primerNombre;
-            this.apellido = chofer.apellidoPaterno;
-            this.SegundoApellido = chofer.apellidoMaterno;
-            this.Licencia = chofer.licenciaConducir;
-            this.Categoria = chofer.categoriaLicencia;
-            this.Telefono = chofer.telefono;
-            this.SegundoNombre = chofer.segundoNombre;
-            this.FechaVencimiento = new Date(chofer.fechaVencimientoLicencia).toLocaleDateString('es-ES', {
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric'
-
-          });
-
-      });
-    });
-    },
-
-    methods: {
-      async fetchChoferes() {
-        const url = 'http://localhost:8069/api/choferes/listar';  // Reemplaza esto con la URL de tu API
-        const response = await axios.get(url);
-        this.choferes = response.data;
-      },
-      //cargar combobox con los id de los choferes
-    cargarChoferes() {
-      axios.get('http://localhost:8069/api/choferes/listar')
-        .then((response) => {
-          response.data.forEach((chofer) => {
-            const option = document.createElement('option');
-            option.value = chofer.id;
-            option.text = chofer.id;
-            document.querySelector('select').appendChild(option);
-          });
-        });
-    },
-    },
-
-    //cargar los datos del chofer seleccionado
-    created() {
-      this.fetchChoferes();
-      this.cargarChoferes();
-    },
-
-
-
-
-  };
-  </script>
-  <style scoped>
-  
-  .portrait {
+<style scoped>
+.portrait {
   width: 100px;
   height: 100px;
   border-radius: 50%;
   margin-top: 20px;
-  background-image: url('../../assets/styles/St_ChoferesVehiculosView/Perfil.jpg');
-  background-size: cover; /* Asegura que la imagen cubra completamente el área sin perder sus proporciones */
-  background-position: center; /* Centra la imagen en el elemento */
+  background-size: cover;
+  background-position: center;
 }
 
 .driver-license {
@@ -182,6 +110,7 @@
   height: 100px;
   border-radius: 50%;
 }
+
 .center-container {
   display: flex;
   flex-direction: column;
@@ -189,6 +118,3 @@
   justify-content: center;
 }
 </style>
-  
-  
-  
