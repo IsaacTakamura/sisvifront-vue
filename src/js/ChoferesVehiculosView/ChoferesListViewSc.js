b9d6786514bc3cb455267005793d81f49f28cc62
Isaac <isaactakamura107@gmail.com>
2024-07-16 14:13:58 -0500

    Separando el codigo de ChoferesListView

---
+++ src/js/ChoferesVehiculosView/ChoferesListViewSc.js
@@ -0,0 +1,37 @@
+export default {
+    props: {
+      search: {
+        type: String,
+        required: true,
+      },
+      choferes: {
+        type: Array,
+        required: true,
+        default: () => [],
+      },
+    },
+    data() {
+      return {
+        localSearch: this.search,
+      };
+    },
+    computed: {
+      filteredChoferes() {
+        return this.choferes.filter((chofer) => {
+          return chofer.primerNombre
+            .toLowerCase()
+            .includes(this.localSearch.toLowerCase());
+        });
+      },
+    },
+    methods: {
+      seleccionarChofer(chofer) {
+        this.$emit("mostrar-datos", chofer);
+      },
+      handleInput(event) {
+        this.localSearch = event.target.value;
+        this.$emit("update:search", this.localSearch);
+      },
+    },
+  };
+  
--- src/views/M.ListadoChoferesVehivulos/ChoferesListView.vue
+++ src/views/M.ListadoChoferesVehivulos/ChoferesListView.vue
@@ -1,4 +1,3 @@
-<!-- Pag.Name= ChoferesListView.vue -->
 <template>
   <div>
     <input
@@ -21,74 +20,5 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    search: {
-      type: String,
-      required: true,
-    },
-    choferes: {
-      type: Array,
-      required: true,
-      default: () => [],
-    },
-  },
-  data() {
-    return {
-      localSearch: this.search,
-    };
-  },
-  computed: {
-    filteredChoferes() {
-      return this.choferes.filter((chofer) => {
-        return chofer.primerNombre
-          .toLowerCase()
-          .includes(this.localSearch.toLowerCase());
-      });
-    },
-  },
-  methods: {
-    seleccionarChofer(chofer) {
-      this.$emit("mostrar-datos", chofer);
-    },
-    handleInput(event) {
-      this.localSearch = event.target.value;
-      this.$emit("update:search", this.localSearch);
-    },
-  },
-};
-</script>
-
-<style scoped>
-.form-control {
-  width: 100%;
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-
-.list-group-item {
-  cursor: pointer;
-  padding: 8px;
-}
-
-.table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-.table th,
-.table td {
-  padding: 8px;
-  border: 1px solid #ccc;
-}
-
-.table-light {
-  background-color: #f8f9fa;
-}
-
-.table-group-divider tr:nth-child(even) {
-  background-color: #f8f9fa;
-}
-</style>
+<script src="@/js/ChoferesVehiculosView/ChoferesListViewSc.js"></script>
+<style src="@/assets/styles/St_ChoferesListView/ChoferesListViewST.css"></style>
